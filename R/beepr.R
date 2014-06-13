f3c1f334e699035bb422e61588dd3201f850eec1
Rasmus Bååth <rasmus.baath@gmail.com>
2014-06-13 00:00:00 +0000

    version 1.0

---
+++ R/beepr.R
@@ -0,0 +1,108 @@
+#'Play a short sound
+#'
+#'\code{beep} plays a short sound which is useful if you want to get notified, 
+#'for example, when a script has finished. As an added bonus there are a number 
+#'of different sounds to choose from.
+#'
+#'
+#'@param sound character string or number specifying what sound to be played by either
+#' specifying one of the built in sounds or specifying the path to a wav file.
+#'  The default is 1. Possible sounds are: \enumerate{
+#'   \item \code{"ping"}
+#'   \item \code{"coin"}
+#'   \item \code{"fanfare"}
+#'   \item \code{"complete"}
+#'   \item \code{"treasure"}
+#'   \item \code{"ready"}
+#'   \item \code{"shotgun"}
+#'   \item \code{"mario"}
+#'   \item \code{"wilhelm"}
+#'   \item \code{"facebook"}
+#' } If \code{sound} does not match any of the sounds above, or is a valid path, a random
+#' sound will be played.
+#' @param expr An optional expression to be excecuted before the sound.
+#'   
+#'   
+#' @return NULL
+#'   
+#' @examples
+#' # Play a "ping" sound
+#' beep()
+#' 
+#' \dontrun{
+#' # Play a fanfare instead of a "ping".
+#' beep("fanfare")
+#' # or
+#' beep(3)
+#' 
+#' # Play a random sound
+#' beep(0)
+#' 
+#' # Update all packages and "ping" when it's ready
+#' update.packages(ask=FALSE); beep()
+#' }
+#' @export
+beep <- function(sound=1, expr=NULL) {
+  expr
+  sounds <- c(ping = "microwave_ping_mono.wav",
+              coin = "smb_coin.wav",
+              fanfare = "victory_fanfare_mono.wav",
+              complete = "work_complete.wav",
+              treasure = "new_item.wav",
+              ready = "ready_master.wav",
+              shotgun = "shotgun.wav",
+              mario = "smb_stage_clear.wav",
+              wilhelm = "wilhelm.wav",
+              facebook = "facebook.wav")
+  if(is.na(sounds[sound]) || length(sounds[sound]) != 1) {
+    if(is.character(sound) && file.exists(sound)) {
+      sound_path <- sound
+    } else{
+      sound_path <- system.file(paste("sounds/", sample(sounds, size=1), sep=""), package="beepr")
+    }
+  } else {
+    sound_path <- system.file(paste("sounds/", sounds[sound], sep=""), package="beepr")
+  }
+  play_file(sound_path)
+}
+
+is_wav_fname <- function(fname) {
+  str_detect(fname, ignore.case("\\.wav$"))
+}
+
+play_vlc <- function(fname) {
+  system(paste("vlc -Idummy --no-loop --no-repeat --playlist-autostart --no-media-library --play-and-exit", fname), 
+         ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_paplay <- function(fname) {
+  system(paste("paplay ", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_aplay <- function(fname) {
+  system(paste("aplay --buffer-time=48000 -N -q", fname), ignore.stdout = TRUE, ignore.stderr=TRUE,wait = FALSE)
+  invisible(NULL)
+}
+
+play_audio <- function(fname) {
+  sfx <- load.wave(fname)
+  play(sfx)
+}
+
+play_file <- function(fname) {
+  if(Sys.info()["sysname"] == "Linux") {
+     if(is_wav_fname(fname) && nchar(Sys.which("paplay")) >= 1) {
+      play_paplay(fname)
+    } else if(is_wav_fname(fname) && nchar(Sys.which("aplay")) >= 1) {
+      play_aplay(fname)
+    } else if(nchar(Sys.which("vlc")) >= 1) {
+      play_vlc(fname)
+    } else {
+      play_audio(fname)
+    }
+  } else {
+    play_audio(fname)
+  }
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+`beepr`: Easily Play Notification Sounds on any Platform
+========================================================
+
+`beepr` is an R package that contains one function, `beep()`, with one purpose: To make it easy to play notification sounds on whatever platform you are on. It is intended to be useful, for example, if you are running a long analysis in the background and want to know when it is ready.
+
+Installation
+----------------
+
+`beepr` is available on [CRAN](http://cran.r-project.org/) and can be installed from within R by running:
+
+```
+install.packages("beepr")
+```
+
+Requirements
+---------------
+
+If you are using Windows or OS X `beepr` relies on the `audio` package for sound playback and no external program is needed.
+
+If you're on Linux `beepr` relies on you having either the `paplay` utility from the Pulse Audio system, the `aplay` utility from the ALSA system or [VLC media player](http://www.videolan.org/vlc/index.html) installed and on the PATH. Chances are that you alread have one of these. If you are on Debian/Ubuntu you can otherwise get VLC by running the following in a terminal:
+
+```
+sudo apt-get install vlc
+```
+
+Details
+------------
+
+`beep()` plays a short sound which is useful if you want to get notified, for example, when a script has finished. As an added bonus there are a number of different sounds to choose from.
+
+### Usage
+
+`beep(sound = 1, expr = NULL)`
+
+### Arguments
+
+`sound`  character string or number specifying what sound to be played by either specifying one of the built in sounds or specifying the path to a wav file. The default is 1. Possible sounds are:
+
+1. "ping"
+2. "coin"
+3. "fanfare"
+4. "complete"
+5. "treasure"
+6. "ready"
+7. "shotgun"
+8. "mario"
+9. "wilhelm"
+10. "facebook"
+
+If `sound` does not match any of the sounds above, or is a valid path, a random sound will be played.
+
+`expr`	An optional expression to be executed before the sound.
+
+### Examples
+
+```
+# Update all packages and "ping" when it's ready
+update.packages(ask=FALSE); beep()
+
+#Play a fanfare instead of a "ping".
+beep("fanfare")
+#or
+beep(3)
+
+# Play a random sound
+beep(0)
+```
